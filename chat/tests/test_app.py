2c59137614e22c43f5f2a532a93b5766deb26938
keshav.sharma <keshav.sharma@arkasoftwares.com>
2021-06-30 16:32:03 +0530

    umang deploy to heroku again

---
+++ chat/tests/test_app.py
@@ -0,0 +1,7 @@
+# from chat.views import index
+# from django.test import Client
+
+# client = Client()
+# def test_index():
+#     response = client.get('/')
+#     assertTemplateUsed(response, 'chat/index.html')
